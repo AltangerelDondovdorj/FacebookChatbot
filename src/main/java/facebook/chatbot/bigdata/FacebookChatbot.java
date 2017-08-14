f5cd527f78dd0191b767d79034f80e975584a71a
Aagii <adondovdorj@mum.edu>
2017-08-14 15:40:43 -0500

    firstCommit of Facebook chatbot

---
+++ src/main/java/facebook/chatbot/bigdata/FacebookChatbot.java
@@ -0,0 +1,13 @@
+package facebook.chatbot.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class FacebookChatbot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookChatbot.class, args);
+	}
+	
+}
---
+++ src/main/java/facebook/chatbot/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package facebook.chatbot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class WebSecurityConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/*.html").addResourceLocations("/");
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+	    registry.addViewController("/").setViewName("forward:/index.html");
+	}
+}
